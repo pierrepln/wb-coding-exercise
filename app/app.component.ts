a65125a0a3f7c9432c8b20efe70b2ebef8b15a8d
Pierre Plantié <pierrepl@ntie.fr>
2016-12-29 18:12:20 +0000

    add subline

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>Explore any city</h1>
+    <p>Discover the greatest venues in any city in the world</p>
     <search></search>
     <result></result>
   `
